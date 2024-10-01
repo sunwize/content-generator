775afa6b1a9a5c27eafa0c9c0952ae8419c2d610
Colin <colinclissonhg@gmail.com>
2024-09-30 21:53:21 -0400

    feat: add clean code

--- assets/utils/formatter.ts
+++ assets/utils/formatter.ts
@@ -4,8 +4,14 @@ import prettierHTML from "prettier/plugins/html";
 import prettierCSS from "prettier/plugins/postcss";
 import prettier from "prettier/standalone";
 
+const cleanCode = (code: string) => {
+    return code
+        .replace(/<!--[\s\S]*?-->/g, "")
+        .replace(/\/\*[\s\S]*?\*\//g, "");
+};
+
 export const formatCode = async (code: string, parser: string) => {
-    return await prettier.format(code, {
+    return await prettier.format(cleanCode(code), {
         parser,
         plugins: [prettierBabel, prettierEstree, prettierHTML, prettierCSS],
     });
